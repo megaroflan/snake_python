36bd1113858a961983153c8d1c4ee4b26be1530b
megaroflan <r00075@rambler.ru>
2024-03-17 22:48:16 +0500

    food spawn added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 def out_of_border(x: int, y: int) -> bool:
@@ -12,10 +13,11 @@ def start_game():
 
     x_delta, y_delta = (0, 0)
 
-    snake_speed = 2.5
-    snake_size = 10
     snake_tail = list()
     snake_len = 1
+
+    food_x = random.randrange(0, display_width // snake_size - 1) * snake_size
+    food_y = random.randrange(0, display_height // snake_size - 1) * snake_size
 
     game_over = False
     while not game_over:
@@ -26,13 +28,18 @@ def start_game():
                 case pygame.KEYDOWN:
                     match event.key:
                         case pygame.K_LEFT:
-                            x_delta, y_delta = (-snake_speed, 0)
+                            if (x_delta, y_delta) != TURN_RIGHT:
+                                x_delta, y_delta = TURN_LEFT
                         case pygame.K_RIGHT:
-                            x_delta, y_delta = (snake_speed, 0)
+                            if (x_delta, y_delta) != TURN_LEFT:
+                                x_delta, y_delta = TURN_RIGHT
                         case pygame.K_UP:
-                            x_delta, y_delta = (0, -snake_speed)
+                            if (x_delta, y_delta) != TURN_DOWN:
+                                x_delta, y_delta = TURN_UP
                         case pygame.K_DOWN:
-                            x_delta, y_delta = (0, snake_speed)
+                            if (x_delta, y_delta) != TURN_UP:
+                                x_delta, y_delta = TURN_DOWN
+
         head_x += x_delta
         head_y += y_delta
 
@@ -41,9 +48,16 @@ def start_game():
 
         display.fill(green)
         pygame.draw.rect(display, blue, [head_x, head_y, snake_size, snake_size])
+        pygame.draw.rect(display, red, [food_x, food_y, snake_size, snake_size])
         pygame.display.update()
 
-        clock.tick(144)
+        if abs(head_x - food_x) < snake_size and abs(head_y - food_y) < snake_size:
+            print('food was eaten')
+            food_x = round(random.randrange(0, display_width - snake_size) / 10.0) * 10.0
+            food_y = round(random.randrange(0, display_height - snake_size) / 10.0) * 10.0
+
+        clock.tick(60)
+
     pygame.quit()
     quit()
 
@@ -57,6 +71,13 @@ display_width = 800
 display_height = 600
 display = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Snake')
+
+snake_speed = 2.5
+snake_size = 10
+TURN_LEFT = (-snake_speed, 0)
+TURN_RIGHT = (snake_speed, 0)
+TURN_UP = (0, -snake_speed)
+TURN_DOWN = (0, snake_speed)
 
 clock = pygame.time.Clock()
 
